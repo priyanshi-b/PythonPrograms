0f24f38572bb461d9ffc02461484c121ff059748
PRIYANSHI-BANSAL <priyanshi0410bansal@gmail.com>
2025-09-10 12:35:31 +0530

    Add files via upload

---
+++ sheet4 pattern/ques4.py
@@ -0,0 +1,8 @@
+n = 5
+print("* " * n)
+
+for i in range(n - 2):
+    print("* " * 2)
+
+
+print("* " * n)
---
+++ sheet4 pattern/ques5.py
@@ -0,0 +1,7 @@
+n = 5   
+
+for i in range(1, n + 1):
+    if i == 3:
+        print("*")
+    else:
+        print("* *")
